8a1d7c21fd26e980123113a54eab42154c65c1c4
Ritesh Tiwari <riteshtiwari195@gmail.com>
2023-02-26 21:51:58 +0530

    add comment

--- employeeapi/employeeapp/serializers.py
+++ employeeapi/employeeapp/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from employeeapp.models import Company
 
 #create serializers here
+#apiadd
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     class meta:
